923ab892c056fef6e384c8878d112bc57269116f
SnehKalariya <snehkalariya9583@gmail.com>
2023-08-21 16:21:09 +0530

    Initial Commit

---
+++ stackProject/stackProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  stackProjectApp.swift
+//  stackProject
+//
+//  Created by Sneh Kalariya on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct stackProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
